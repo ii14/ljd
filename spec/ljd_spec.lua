5c5a6ec2eabe77f3b7468c17126ea3bb99b3d45c
ii14 <ii14@users.noreply.github.com>
2022-07-09 03:04:51 +0200

    spec update

--- spec/ljd_spec.lua
+++ spec/ljd_spec.lua
@@ -3,66 +3,84 @@ local ljd = require('ljd')
 local eq = assert.is_same
 
 local function getline(f)
-  return assert(assert(debug.getinfo(f, 'S')).linedefined)
+  local info = assert(debug.getinfo(f, 'S'), 'debug.getinfo')
+  return assert(info.linedefined, 'debug.getinfo linedefined')
 end
 
 describe('ljd', function()
-  it('can step() through function', function()
-    local function f()
-      local a = 1
-      local b = 2
-      return a, b
-    end
+  describe('step()', function()
+    it('can step through function', function()
+      local function f()
+        local a = 1
+        local b = 2
+        return a, b
+      end
 
-    local line = getline(f)
-    local D = ljd.new(f)
+      local D, L = ljd.new(f), getline(f)
 
-    eq(-1, D.currentline)
-    eq({ 0 }, { D:step() })
-    eq(line + 1, D.currentline)
-    eq({ 0 }, { D:step() })
-    eq(line + 2, D.currentline)
-    eq({ 0 }, { D:step() })
-    eq(line + 3, D.currentline)
-    eq({ true, 1, 2 }, { D:step() })
-    eq(line + 3, D.currentline)
+      eq(-1, D.currentline)
+      eq({ 0 }, { D:step() })
+      eq(L + 1, D.currentline)
+      eq({ 0 }, { D:step() })
+      eq(L + 2, D.currentline)
+      eq({ 0 }, { D:step() })
+      eq(L + 3, D.currentline)
+      eq({ true, 1, 2 }, { D:step() })
+    end)
   end)
 
-  it('can next() through function', function()
-    local function f()
-      local a = 1
-      local b = 2
-      return a, b
-    end
+  describe('next()', function()
+    it('can step through function', function()
+      local function f()
+        local a = 1
+        local b = 2
+        return a, b
+      end
 
-    local line = getline(f)
-    local D = ljd.new(f)
+      local D, L = ljd.new(f), getline(f)
 
-    eq(-1, D.currentline)
-    eq({ 0 }, { D:step() })
-    eq(line + 1, D.currentline)
-    eq({ 0 }, { D:next() })
-    eq(line + 2, D.currentline)
-    eq({ 0 }, { D:next() })
-    eq(line + 3, D.currentline)
-    eq({ true, 1, 2 }, { D:next() })
-    eq(line + 3, D.currentline)
+      eq(-1, D.currentline)
+      eq({ 0 }, { D:step() })
+      eq(L + 1, D.currentline)
+      eq({ 0 }, { D:next() })
+      eq(L + 2, D.currentline)
+      eq({ 0 }, { D:next() })
+      eq(L + 3, D.currentline)
+      eq({ true, 1, 2 }, { D:next() })
+    end)
   end)
 
-  it('can finish()', function()
-    local function f()
-      local a = 1
-      local b = 2
-      return a, b
-    end
+  describe('finish()', function()
+    it('can resume execution', function()
+      local function f()
+        local a = 1
+        local b = 2
+        return a, b
+      end
 
-    local line = getline(f)
-    local D = ljd.new(f)
+      local D, L = ljd.new(f), getline(f)
 
-    eq(-1, D.currentline)
-    eq({ 0 }, { D:step() })
-    eq(line + 1, D.currentline)
-    eq({ true, 1, 2 }, { D:finish() })
-    eq(line + 3, D.currentline)
+      eq(-1, D.currentline)
+      eq({ 0 }, { D:step() })
+      eq(L + 1, D.currentline)
+      eq({ true, 1, 2 }, { D:finish() })
+    end)
+  end)
+
+  describe('continue()', function()
+    it('can resume execution', function()
+      local function f()
+        local a = 1
+        local b = 2
+        return a, b
+      end
+
+      local D, L = ljd.new(f), getline(f)
+
+      eq(-1, D.currentline)
+      eq({ 0 }, { D:step() })
+      eq(L + 1, D.currentline)
+      eq({ true, 1, 2 }, { D:continue() })
+    end)
   end)
 end)
